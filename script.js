fa3c9a3d5caf054bc54818dded346fd40b2f896e
User Name <nguyenducphi002@gmail.com>
2023-01-10 11:54:46 +0700

    comments

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+/* các dòng const khai báo các biến tương tác với các tên id bên html bằng cú pháp
+ documet.getElementById("tên id bên html")*/
 const timeEl = document.getElementById('time');
 const dateEl = document.getElementById('date');
 const currentWeatherItemsEl = document.getElementById('current-weather-items');
@@ -6,157 +9,224 @@ const timezone = document.getElementById('time-zone');
 const countryEl = document.getElementById('country');
 const weatherForecastEl = document.getElementById('weather-forecast');
 const currentTempEl = document.getElementById('current-temp');
-const btn_find_cityname =document.getElementById('btn_find_city_name')
-const inputnamecity =document.getElementById('input_city_name');
-const matchlist =document.getElementById('match-list');
-const btngetitem =document.getElementById("btnitemsearch");
-const xemchitiet =document.getElementById("btnxemchitiet");
-const chitietscroll=document.getElementById("scrollmenuchitiet");
+const btn_find_cityname = document.getElementById('btn_find_city_name')
+const inputnamecity = document.getElementById('input_city_name');
+const matchlist = document.getElementById('match-list');
+const btngetitem = document.getElementById("btnitemsearch");
+const xemchitiet = document.getElementById("btnxemchitiet");
+const chitietscroll = document.getElementById("scrollmenuchitiet");
 const thongtinchitiet = document.getElementById("exampleModalCenterTitle");
 const days = ['Chủ Nhật', 'Thứ 2', 'Thứ 3', 'Thứ 4', 'Thứ 5', 'Thứ 6', 'Thứ 7']
 const months = ['Tháng 1', 'Tháng 2', 'Tháng 3', 'Tháng 4', 'Tháng 5', 'Tháng 6', 'Tháng 7', 'Tháng 8', 'Tháng 9', 'Tháng 10', 'Tháng 11', 'Tháng 12'];
 
-const API_KEY ='1779ca818b59557aa48558aec376d6db';
+/* const API_KEY này là key được đăng ký trên website openweathermap để sử dụng các dữ liệu thời tiết trả về
+thông qua key này*/
+const API_KEY = '1779ca818b59557aa48558aec376d6db';
 
-var long,lat,times;
 
+var long, lat, times;
+
+/*hàm setInterval này lấy ngày giờ tháng năm thực*/
 setInterval(() => {
     const time = new Date();
     const month = time.getMonth();
     const date = time.getDate();
     const day = time.getDay();
     const hour = time.getHours();
-    const hoursIn12HrFormat = hour >= 13 ? hour %12: hour
+    const hoursIn12HrFormat = hour >= 13 ? hour % 12 : hour
     const minutes = time.getMinutes();
-    const ampm = hour >=12 ? 'PM' : 'AM'
+    const ampm = hour >= 12 ? 'PM' : 'AM'
 
-    timeEl.innerHTML = (hoursIn12HrFormat < 10? '0'+hoursIn12HrFormat : hoursIn12HrFormat) + ':' + (minutes < 10? '0'+minutes: minutes)+ ' ' + `<span id="am-pm">${ampm}</span>`
+    timeEl.innerHTML = (hoursIn12HrFormat < 10 ? '0' + hoursIn12HrFormat : hoursIn12HrFormat) + ':' + (minutes < 10 ? '0' + minutes : minutes) + ' ' + `<span id="am-pm">${ampm}</span>`
 
-    dateEl.innerHTML = days[day] + ', ' + date+ ' ' + months[month]
-    times = days[day] + ', ' + date+ ' ' + months[month];
+    dateEl.innerHTML = days[day] + ', ' + date + ' ' + months[month]
+    times = days[day] + ', ' + date + ' ' + months[month];
 }, 1000);
-window.addEventListener("load",()=>{
-    getWeatherData ();
+
+// khi chương trình được load chạy thì nó gọi đén hàm getWeatherData()
+window.addEventListener("load", () => {
+    getWeatherData();
 });
 
-const searchStates =async searchText =>{
-    //const res = await  fetch('https://github.com/ndphii/do_an_lap_trinh_mang/blob/master/data/citylist.json', { mode: 'no-cors'});
+// trạng thái của thanh tìm kiếm khi gõ các ký tự được khái báo là một hàm bất đồng bộ với cú pháp là async
+const searchStates = async searchText => {
 
-    const res = await  fetch('../data/citylist.json');
+    // lấy các dữ liệu trong file json (trong data/citylist.json)
+    const res = await fetch('../data/citylist.json');
+
+    /* res.json Nó trả về một lời hứa sẽ giải quyết bằng kết quả phân tích cú pháp văn bản nội dung dưới dạng JSON.
+    từ biến res mà nó đã xử lý ở trên,từ đó nó lấy ra đươc 1 mảng dữ liệu được lưu vào viến states*/
     const states = await res.json();
-    let matches= states.filter(state =>{
-        const regex = new RegExp(`^${searchText}`,'gi');
+
+    /* filter nó giống như vòng lặp for với 1 cái if để lấy ra những phần tử đạt điều kiện (if) 
+    nó sẽ lọc ra các giá trị tên thầnh phố gần giống với cú pháp mình input vào form tìm kiếm từ 
+    mảng states ở trên */
+    let matches = states.filter(state => {
+        /*
+        RegExp ở đây là biểu thức chính quy với cú pháp ^${giá trị truyền vào} dấu ^ là bắt đầu của từ khoá nhập vào
+        với thông số 'gi' ở đây là:
+        i là so khớp không quan tâm đến chữ hoa chữ thường
+        còn g là so khớp toàn bộ chuỗi cần tìm
+         */
+        const regex = new RegExp(`^${searchText}`, 'gi');
+
+        /* state.name.match(regex) ở đây là gọi name trong file json thông qua biến lưu mảng json là 
+         state còn match là sẽ tìm kiếm các chuỗi con phù hợp với biểu thức chính quy ( là biến regex) được cung cấp
+         tương tự state.country.match(regex) sẽ lấy country
+        */
         return state.name.match(regex) || state.country.match(regex);
     });
-    if(searchText.length === 0){
+
+    // nếu không gõ ký tự nào thì matchlist sẽ ko hiện các dữ liệu gợi ý
+    if (searchText.length === 0) {
         matches = [];
-        matchlist.innerHTML ='';
+        // với matchlist ở đây là một tên biến id ở bên html được khai báo ở dòng 14 phía trên 
+        matchlist.innerHTML = '';
     }
-     outputHtml(matches);
+    // truyền các tên thành phố tương ứng được gợi ý vào hàm outputhtml
+    outputHtml(matches);
 }
- const outputHtml = matches =>{
-    if (matches.length >0 ){
+
+// hàm này đưa các dữ liệu tìm kiếm được gợi ý từ hàm phía trên qua bên html cho nó hiễn thị
+const outputHtml = matches => {
+    /* nếu dữ liệu tự matches có được gợi ý tên thành phố thì từng tên thành phố sẽ được add như một button để khi bấm vào
+      dữ liệu thời tiết sẽ nhảy đến đó */
+    if (matches.length > 0) {
         const html = matches.map(match =>
-        `<button onclick="getnamecitybybutton(this)" id="btnitemsearch" type="button" class="list-group-item list-group-item-action" value="${match.name}">
+            // mỗi cái tên gợi ý được nằm trong 1 button
+            `<button onclick="getnamecitybybutton(this)" id="btnitemsearch" type="button" class="list-group-item list-group-item-action" value="${match.name}">
             ${match.name} <span>(${match.country})</span>
         </button>
         `
         ).join('');
-        matchlist.innerHTML =html;
+        matchlist.innerHTML = html;
     }
 }
+// nếu thanh tìm kiếm được input ký tự thì gọi đến searchStates và truyền ký tự mình gõ vào để nó gợi ý
+input_city_name.addEventListener('input', () => searchStates(input_city_name.value));
 
-input_city_name.addEventListener('input',()=> searchStates(input_city_name.value));
-xemchitiet.addEventListener('click',()=>{
+/* nếu bấm vào nút xem chi tiết thì nó sẽ thử gọi hàm getchitiet với 2 tham số long lat ở đây là kinh tuyến và vĩ tuyến
+nhằm trả về vị trí vệ tinh để lấy dữ liệu thời tiết tại vị trí đó */
+xemchitiet.addEventListener('click', () => {
     try {
-        getchitiet(long,lat);
+        getchitiet(long, lat);
     } catch (error) {
-        
+
     }
 });
-btn_find_cityname.addEventListener('click',()=>{
-    if(inputnamecity.value===''){
-       alert("không để trống")        
-    }else{
+// nút tìm kiếm tên thành phố
+btn_find_cityname.addEventListener('click', () => {
+    if (inputnamecity.value === '') {
+        alert("không để trống")
+    } else {
         getWeatherDataByCityName(inputnamecity.value);
         searchStates('');
-        inputnamecity.value='';
+        inputnamecity.value = '';
     }
 });
-function getnamecitybybutton(city){
+
+function getnamecitybybutton(city) {
     getWeatherDataByCityName(city.value);
-    inputnamecity.value="";
+
+    // 2 dòng này sẽ xoá các dữ liệu gợi ý khi mình đã nhấn nút tìm kiếm tên 
+    inputnamecity.value = "";
     matchlist.innerHTML = "";
 }
 function getWeatherDataByCityName(city) {
-        fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&lang=vi&appid=${API_KEY}`).then(res => res.json()).then(data => {
+    /*
+     fetch ni trỏ tới ... còn https://api.open.... ni là cú pháp mà website đăng ký dữ liệu api cũng cấp
+     bắt buộc phải gõ đúng như nó yêu cầu link đọc dữ liệu https://openweathermap.org/forecast5
+    */
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&lang=vi&appid=${API_KEY}`).then(res => res.json()).then(data => {
         showWeatherData(data);
-        long= data.city.coord.lon;
+        // lấy giá trị kinh tuyến vĩ tuyến lại tí dùng cho hàm khác
+        long = data.city.coord.lon;
         lat = data.city.coord.lat;
-        })
+    })
 }
-function getWeatherData () {
+function getWeatherData() {
+    // navigator.geolocation.getCurrentPosition này hiểu đơn giản là dùng để định vị vị trí
     navigator.geolocation.getCurrentPosition((success) => {
-        let {latitude, longitude } = success.coords;
+        let { latitude, longitude } = success.coords;
         fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&lang=vi&appid=${API_KEY}`).then(res => res.json()).then(data => {
-        console.log(data)
-        long = longitude;
-        lat = latitude;
-        showWeatherData(data);
+            console.log(data)
+            long = longitude;
+            lat = latitude;
+            showWeatherData(data);
         })
     })
 }
-function getbackground(main){
-    var backgroundImage ;
-    if(main==="Clear"){ backgroundImage = document.body.style.backgroundImage = "url('./imgbackground/sun.jpg')"}
-    else if ( main==="Clouds"){backgroundImage = document.body.style.backgroundImage = "url('./imgbackground/cloud.jpg')"}
-    else if ( main==="Rain"){backgroundImage = document.body.style.backgroundImage = "url('./imgbackground/rain.jpg')"}
-    else if ( main==="Snow"){backgroundImage = document.body.style.backgroundImage = "url('./imgbackground/snow.jpg')"}
-    else if ( main==="Thunderstorm"){backgroundImage = document.body.style.backgroundImage = "url('./imgbackground/thunder.jpg')"}
-    else{}
+
+/* hàm này sẽ lấy background mưa hay nắng hay gió hay tuyết rơi ... tuỳ vào "main" với main ở đây là trạng thái thời
+tiết chính được trả về từ API với description ví dụ như trời nắng,trời quang đảng, hay trời mưa.... */
+function getbackground(main) {
+    var backgroundImage;
+    if (main === "Clear") { backgroundImage = document.body.style.backgroundImage = "url('./imgbackground/sun.jpg')" }
+    else if (main === "Clouds") { backgroundImage = document.body.style.backgroundImage = "url('./imgbackground/cloud.jpg')" }
+    else if (main === "Rain") { backgroundImage = document.body.style.backgroundImage = "url('./imgbackground/rain.jpg')" }
+    else if (main === "Snow") { backgroundImage = document.body.style.backgroundImage = "url('./imgbackground/snow.jpg')" }
+    else if (main === "Thunderstorm") { backgroundImage = document.body.style.backgroundImage = "url('./imgbackground/thunder.jpg')" }
+    else { }
     return backgroundImage;
 }
-function showWeatherData (data){
-    let {sunrise, sunset} = data.city;
-    let {feels_like,humidity,sea_level,pressure} = data.list[0].main;
-    let {main,description,icon} = data.list[0].weather[0];
-    let {speed} = data.list[0].wind;
-    var nhietdo = Math.floor(feels_like-273);
-    var Numbertempmax,Numbertempmin;
-    Numbertempmax = Math.floor(data.list[0].main.feels_like-273);
-    Numbertempmin = Math.floor(data.list[0].main.feels_like-273);
-    var nhietdocaonhat ;
-    var nhietdothapnhat ;
-    var rains=0;
-    timezone.innerHTML = data.city.country + " / " +data.city.name ;
-    countryEl.innerHTML = data.city.coord.lat + 'N  - ' + data.city.coord.lon+'E';
-    if(main==="Rain"){
+function showWeatherData(data) {
+    /* các biến như sunrise,sunset feels_like này ko được đặc ngẫu nhiên theo sỡ thích mà nó được lấy từ
+     object muốn biết rõ thêm thì chạy web lên rồi bấm f12 ta sẽ thấy như này {cod: '200', message: 0, cnt: 40, list: Array(40), city: {…}
+     xổ đoạn này ra thì t sẽ thấy các tên biến trong đó t gọi nó ra để lấy dữ liệu link ảnh minh hoạ
+     https://scontent.fsgn2-7.fna.fbcdn.net/v/t1.15752-9/323841135_1731231480605204_6729238803081255878_n.png?_nc_cat=100&ccb=1-7&_nc_sid=ae9488&_nc_ohc=nXyH5IkuoGAAX-DjySf&_nc_ht=scontent.fsgn2-7.fna&oh=03_AdQ3wbs-y5YtyNkTkKPbQo1stam3vpa_hHJGmqh1QmiH7g&oe=63E46046
+    */
+    let { sunrise, sunset } = data.city;
+    let { feels_like, humidity, sea_level, pressure } = data.list[0].main;
+    let { main, description, icon } = data.list[0].weather[0];
+    let { speed } = data.list[0].wind;
+    var nhietdo = Math.floor(feels_like - 273);
+    var Numbertempmax, Numbertempmin;
+    Numbertempmax = Math.floor(data.list[0].main.feels_like - 273);
+    Numbertempmin = Math.floor(data.list[0].main.feels_like - 273);
+    var nhietdocaonhat;
+    var nhietdothapnhat;
+    var rains = 0;
+    timezone.innerHTML = data.city.country + " / " + data.city.name;
+    countryEl.innerHTML = data.city.coord.lat + 'N  - ' + data.city.coord.lon + 'E';
+    /* tại vì không phải lúc nào trời cũng mưa nên API sẽ ko trả dữ liệu lượng mưa 3h trước cho ta
+     nên t phải kiểm tra xem hiện tại thời tiết có đang mưa ("rain") không ? nếu có mưa thì t mới 
+     lấy dữ liệu từ biến x gán cho rain với x là lượng mưa tính bằng milimet 
+    */
+    if (main === "Rain") {
+        /* vì giá trị data.list[0].rain nằm trong một object nên t phải gán nó nằm trong một object
+        nếu khi main không tồn tại "Rain" thì biến x lúc này sẽ bị lỗi , không rỏ lắm đại khái v
+         */
         const x = Object.values(data.list[0].rain);
-        rains=x[0];
+        rains = x[0];// lấy giá trị đầu tiên cũng là giá trị lượng mưa nếu có mưa
     }
-   
-    let otherDayForcast = ''
-    
-    data.list.forEach((day, idx) => {
-        
-        var checktimday = window.moment(day.dt_txt).format('HH:mm:ss');
-        var descriptiondubao =day.weather[0].description;
-        var nhietdotrungbinh =Math.floor(day.main.feels_like-273);
 
-        if(idx <= 12){
-            if(nhietdotrungbinh>=Numbertempmax){
+    let otherDayForcast = ''
+
+    /* duyệt qua từng object trong list đễ hiển thị dự báo  trong 5 ngày tiếp theo */
+    data.list.forEach((day, idx) => {
+
+        var checktimday = window.moment(day.dt_txt).format('HH:mm:ss');
+        var descriptiondubao = day.weather[0].description;
+        var nhietdotrungbinh = Math.floor(day.main.feels_like - 273);
+
+        // duyệt qua 12 object ban đầu xem thử hôm nay nhiệt độ cao nhất,nhỏ nhất là bao nhiều
+        // hàm if ni để lấy giá trị nheietj độ cao nhất và thấp nhất ngày hôm nay
+        if (idx <= 12) {
+
+            if (nhietdotrungbinh >= Numbertempmax) {
                 Numbertempmax = nhietdotrungbinh;
                 nhietdocaonhat = Numbertempmax;
             }
-            if (nhietdotrungbinh<=Numbertempmin){
+            if (nhietdotrungbinh <= Numbertempmin) {
                 Numbertempmin = nhietdotrungbinh;
                 nhietdothapnhat = Numbertempmin;
             }
         }
-        if(idx!=0 && checktimday==="00:00:00") {           
-               otherDayForcast += `
+        // qua 00 giờ là tính ngày khác nên đều kiện đẽ hiển thị cho dữ báo 5 ngày tới là qua 0h là tính một ngày 
+        if (idx != 0 && checktimday === "00:00:00") {
+            otherDayForcast += `
                     <div class="weather-forecast-item">
-                        <div class="day">${window.moment(day.dt*1000).format('ddd DD/MM')}</div>
+                        <div class="day">${window.moment(day.dt * 1000).format('ddd DD/MM')}</div>
                         <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png" alt="weather icon" class="w-icon">
                         <div class="templ">${nhietdotrungbinh}&#176;C</div>
                         <div class="templ">${descriptiondubao}</div>
@@ -165,6 +235,8 @@ function showWeatherData (data){
         }
     })
     getbackground(main);
+
+    // phần này để add dữ liệu vào html rồi cho html hiển thị
     currentTempEl.innerHTML = `
     <img src="http://openweathermap.org/img/wn/${icon}.png" alt="weather icon" class="w-icon">
     <div class="other">
@@ -173,8 +245,8 @@ function showWeatherData (data){
         <div class="templ">Thấp nhất - ${nhietdothapnhat}&#176;C</div>
     </div>
     `
-    nhietdoto.innerHTML = 
-    `<div class="temp">
+    nhietdoto.innerHTML =
+        `<div class="temp">
         <div class="textnhiet">${nhietdo}</div>
         <div class="textdoc">°C</div>
     </div>
@@ -186,9 +258,9 @@ function showWeatherData (data){
         Nhiệt độ cao nhất :${nhietdocaonhat}°C  -  Nhiệt độ thấp nhất :${nhietdothapnhat}°C
     </div>
     `
-    ;
-    currentWeatherItemsEl.innerHTML = 
-    `<div class="weather-item">
+        ;
+    currentWeatherItemsEl.innerHTML =
+        `<div class="weather-item">
         <div>Độ ẩm</div>
         <div>${humidity} %</div>
     </div>
@@ -207,7 +279,7 @@ function showWeatherData (data){
     </div>
     <div class="weather-item">
         <div>Hoàng Hôn</div>
-        <div>${window.moment(sunset*1000).format('HH:mm a')}</div>
+        <div>${window.moment(sunset * 1000).format('HH:mm a')}</div>
     </div>
     <div class="weather-item">
         <div>Mực nước biển</div>
@@ -221,22 +293,25 @@ function showWeatherData (data){
     `;
     weatherForecastEl.innerHTML = otherDayForcast;
 
-    
+
 }
- function getchitiet(long,lat){
+function getchitiet(long, lat) {
+    /* lấy thogno tin chi tiết này nó sẽ dùng lại hai  biến kinh tuyến và vĩ tuyến của vị trí hiện tại
+    qua hai biến long và lat*/
+
     fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&lang=vi&appid=${API_KEY}`).then(res => res.json()).then(data => {
-    thongtinchitiet.innerHTML=`
+        thongtinchitiet.innerHTML = `
                   <div class="datec" id="date">${times}  -</div>
                   <div id="country" class="countryc">-  ${data.city.name}</div>
                   <div class="time-zonec" id="time-zone">/(${data.city.country})</div>
     `
-    let listchitietscroll=''
-    data.list.forEach((day, idx) => {
-        var checktime = window.moment(day.dt_txt).format('HH:mm');
-        var nhietdotrungbinh =Math.floor(day.main.feels_like-273);
-        var descriptiondubao =day.weather[0].description;
-        if(idx <= 12){
-            listchitietscroll += `
+        let listchitietscroll = ''
+        data.list.forEach((day, idx) => {
+            var checktime = window.moment(day.dt_txt).format('HH:mm');
+            var nhietdotrungbinh = Math.floor(day.main.feels_like - 273);
+            var descriptiondubao = day.weather[0].description;
+            if (idx <= 12) {
+                listchitietscroll += `
             <div class="nav-item col-xl-2">
                         <div class="card " >
                           <div class="card-body">
@@ -247,8 +322,8 @@ function showWeatherData (data){
                         </div>
                       </div>
             `
-        }
+            }
+        })
+        chitietscroll.innerHTML = listchitietscroll;
     })
-    chitietscroll.innerHTML = listchitietscroll;
-    })
- }
+}
